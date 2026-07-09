c8b6768495d92cd03e151a649be5d04810b2c5dd
Aquilaruspante <calogero.salvaggio3@gmail.com>
2026-07-09 23:30:45 +0100

    fix(togglemenu): works fime on input

--- public/genreFetcher.js
+++ public/genreFetcher.js
@@ -1,9 +1,20 @@
+function firstVisibleItem() {
+    for (let i = 0; i < toggleMenuItems.length; i++) {
+        if (!toggleMenuItems[i].classList.contains('hidden')) return { element: toggleMenuItems[i], index: i };
+    };
+};
+
 genreInput.addEventListener('input', (e) => {
+    manageItemFocus();
     toggleMenuItems.forEach((item) => {
+        item.classList.remove('menu-item-focus');
         if (!item.innerText.includes(e.target.value)) {
             item.classList.add('hidden');
         } else {
             item.classList.remove('hidden');
         };
     });
+    const item = firstVisibleItem();
+    item.element.classList.add('menu-item-focus');
+    counter = item.index + 1;
 });
--- public/genreSelector.js
+++ public/genreSelector.js
@@ -1,16 +1,6 @@
-genreInput.addEventListener('keydown', (e) => {
-    genreInput.placeholder = 'Press Tab to add...';
-
-    if (e.key === 'ArrowDown') {
-        counter++;
-        if (counter === toggleMenuItems.length + 1) counter = 1;
-    };
-
-    if (e.key === 'ArrowUp') {
-        counter--;
-        if (counter <= 0) counter = toggleMenuItems.length;
-    };
-
+function manageItemFocus() {
+    if (counter >= toggleMenuItems.length + 1) counter = 1;
+    if (counter <= 0) counter = toggleMenuItems.length;
     toggleMenuItems.forEach((item, index) => {
         if (index + 1 === counter) { 
             item.classList.add('menu-item-focus');
@@ -18,6 +8,24 @@ genreInput.addEventListener('keydown', (e) => {
             item.classList.remove('menu-item-focus');
         };
     });
+};
+
+genreInput.addEventListener('keydown', (e) => {
+    genreInput.placeholder = 'Press Tab to add...';
+
+    if (e.key === 'ArrowDown') {
+        counter++;
+        if (counter >= toggleMenuItems.length + 1) counter = 1;
+        if (toggleMenuItems[counter - 1].classList.contains('hidden')) counter++;   
+    };
+
+    if (e.key === 'ArrowUp') {
+        counter--;
+        if (counter <= 0) counter = toggleMenuItems.length;
+        if (toggleMenuItems[counter - 1].classList.contains('hidden')) counter--;
+    };
+
+    manageItemFocus();
 });
 
 genreInput.addEventListener('keydown', (e) => {
