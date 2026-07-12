1598996ce6bcd90777cf8341eaf4454b771f9d62
Aquilaruspante <calogero.salvaggio3@gmail.com>
2026-07-12 12:28:10 +0100

    add closeButton SVG

--- public/addgenre.js
+++ public/addgenre.js
@@ -1,3 +1,19 @@
+const closeButtonSVG = `
+    <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="13" height="13" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        stroke="currentColor" 
+        stroke-width="2" 
+        stroke-linecap="round" 
+        stroke-linejoin="round" 
+        class="lucide lucide-x-icon lucide-x">
+        <path d="M18 6 6 18"/>
+        <path d="m6 6 12 12"/>
+    </svg>    
+`
+
 function createGenreComponent(value) {
     // create display genre button
     const newElement = document.createElement('div');
@@ -10,7 +26,7 @@ function createGenreComponent(value) {
     newElement.classList.add('genre-display-element');
     closeButton.classList.add('close-button');
     newTextElement.innerText = value;
-    closeButton.innerHTML = '<img src="/Close.png" />';
+    closeButton.innerHTML = closeButtonSVG;
     return newElement;    
 };
 
