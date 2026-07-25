2d6395b99bcc03dd863a3b6c4f9141e38d91d1d5
Aquilaruspante <calogero.salvaggio3@gmail.com>
2026-07-25 11:00:16 +0100

    feat(singleFilm): deleteButton linked to singleFilm page

--- public/scripts/deleteButton.js
+++ public/scripts/deleteButton.js
@@ -1,25 +1,25 @@
-const deleteButtons = document.querySelectorAll('.delete-button');
+const deleteButton = document.querySelector('.delete-button');
 const messageElement = document.querySelector('.message');
 
-deleteButtons.forEach(button => {
-    button.addEventListener('click', async () => {
-        const password = prompt('Password required...'); 
-        const id = button.dataset.id;
 
-        const response = await fetch(`/film/${id}/delete`, { 
-            method: 'POST',
-            body: JSON.stringify({ password }),
-            headers: {
-                "Content-Type": "application/json"
-            } ,
-        });
-        const data = await response.json();
-        
-        if (data.status === 'success') {
-            window.location.replace('/');
-        } else if (data.status === 'denied') {
-            messageElement.innerText = data.message;
-            messageElement.classList.remove('hidden');
-        };
+deleteButton.addEventListener('click', async () => {
+    const password = prompt('Password required...'); 
+    const id = deleteButton.dataset.id;
+
+    const response = await fetch(`/film/${id}/delete`, { 
+        method: 'POST',
+        body: JSON.stringify({ password }),
+        headers: {
+            "Content-Type": "application/json"
+        } ,
     });
+    const data = await response.json();
+    
+    if (data.status === 'success') {
+        window.location.replace('/');
+    } else if (data.status === 'denied') {
+        messageElement.innerText = data.message;
+        messageElement.classList.remove('hidden');
+    };
 });
+
